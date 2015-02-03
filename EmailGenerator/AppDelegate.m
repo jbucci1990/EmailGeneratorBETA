489770cd540932734a885c3d9fbeb375f790b32c
JJ Bucci <codehelp123@gmail.com>
2015-02-03 13:18:58 -0500

    Initial Commit

---
+++ EmailGenerator/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  EmailGenerator
+//
+//  Created by JJ Bucci on 2/3/15.
+//  Copyright (c) 2015 JJ Bucci. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
